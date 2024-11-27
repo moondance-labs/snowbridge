6ff98f0c606b02a5577ef5d38e62078f1c059067
NindoK <b.daniele@protonmail.ch>
2024-11-27 19:30:48 +0100

    Didn't add to git

--- contracts/test/Gateway.t.sol
+++ contracts/test/Gateway.t.sol
@@ -1018,17 +1018,12 @@ contract GatewayTest is Test {
 
     bytes private constant FINAL_VALIDATORS_PAYLOAD =
         hex"7015003800000cd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe228eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48";
-    // bytes private constant VALIDATORS_DATA =
-    //     "d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe228eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48";
 
     bytes32[] private VALIDATORS_DATA = [
         bytes32(0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d),
         bytes32(0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22),
         bytes32(0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48)
     ];
-
-    // bytes private constant WRONG_LENGTH_VALIDATORS_DATA =
-    //     "d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe228eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a4";
 
     function createLongOperatorsData() public view returns (bytes32[] memory) {
         bytes32[] memory result = new bytes32[](1001);
