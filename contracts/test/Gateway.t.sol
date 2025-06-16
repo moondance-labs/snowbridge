cf29e9c4d48b1cfeb0962ed0aa5e9d8c6f5b28ee
Evgeny Svirsky <e.svirsky@tut.by>
2025-06-16 13:50:17 +0200

    - Introduce CI testing

--- contracts/test/Gateway.t.sol
+++ contracts/test/Gateway.t.sol
@@ -13,6 +13,7 @@ import {IUpgradable} from "../src/interfaces/IUpgradable.sol";
 import {Gateway} from "../src/Gateway.sol";
 import {MockGateway} from "./mocks/MockGateway.sol";
 import {MockGatewayV2} from "./mocks/MockGatewayV2.sol";
+import {ReantrantAttacker} from "./mocks/ReantrantAttacker.sol";
 import {GatewayProxy} from "../src/GatewayProxy.sol";
 
 import {AgentExecutor} from "../src/AgentExecutor.sol";
