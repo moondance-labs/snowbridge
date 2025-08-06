560a4049dc7aedcf7a56652036b5a210341bccbd
Evgeny Svirsky <e.svirsky@tut.by>
2025-08-06 18:29:08 +0300

    - reverted test

--- relayer/contracts/gateway.go
+++ relayer/contracts/gateway.go
@@ -41,7 +41,6 @@ type InboundMessage struct {
 	Id             [32]byte
 }
 
-
 // MultiAddress is an auto generated low-level Go binding around an user-defined struct.
 type MultiAddress struct {
 	Kind uint8
